ca2481562e026fc6faad039975f3b2ba7ec9bd3c
Keang <skeang@gmail.com>
2016-03-29 13:23:24 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
 if test "$1" = "osx"; then
-  # Install Homebrew and link ssh file from Dropbox
+  # Install Homebrew
   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-  brew install appledoc
-  brew install androidsdk
   brew install tmux
-  # Link all ssh configurations and keys back
-  ln -s /Users/llun/Dropbox/Keys/ssh /Users/llun/.ssh
   # Install nvm and node.js
   curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-  #Copy environments back
 
 elif test "$1" = "debian"; then
   apt-get install build-essential 
