7bd176219ff46f872961ee64992a4020d87542e9
Song Keang <skeang@gmail.com>
2018-10-21 09:30:00 +1100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ if test "$1" = "osx"; then
   # Install Homebrew
   ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
   brew install tmux
-  ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-  # Install nvm and node.js
-  curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
 elif test "$1" = "debian"; then
   apt-get install build-essential
 elif test "$1" = "ubuntu"; then
@@ -16,16 +12,12 @@ elif test "$1" = "ubuntu"; then
   git config --global color.ui auto
 
   apt-get install curl
-
-  #vim
-  git submodule init
-  git submodule update
-  ln -s ~/dotfiles/.vim ~/.vim
-  ln -s ~/dotfiles/.vimrc ~/.vimrc
-elif test "$1" = "rails"; then
-  #rvm
-  \curl -sSL https://get.rvm.io | bash -s stable
-  #heroku
-  wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 fi
 
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+#vim
+git submodule init
+git submodule update
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
