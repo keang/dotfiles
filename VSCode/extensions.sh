191aa6382ea019a715ad7cdb2b2bc88486b6cbeb
Keang <skeang@gmail.com>
2019-09-12 18:03:30 +1000

    Add extensions

--- VSCode/extensions.sh
+++ VSCode/extensions.sh
@@ -1,1 +1,8 @@
+code --install-extension alexkrechik.cucumberautocomplete
+code --install-extension eamodio.gitlens
+code --install-extension Kelvin.vscode-sshfs
+code --install-extension misogi.ruby-rubocop
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension noku.rails-run-spec-vscode
+code --install-extension tht13.html-preview-vscode
 code --install-extension vscodevim.vim
